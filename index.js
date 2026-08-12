a894ff338e4ff4f11b3859750b2478ba8ca358da
benabasiodiong1-collab <benabasiodiong1@gmail.com>
2026-08-12 07:01:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ http.createServer((req, res) => {
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const geminiModel = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+const geminiModel = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 const OWNER_ID = process.env.OWNER_TELEGRAM_ID;
 
 const DB_FILE = "./leads.json";
